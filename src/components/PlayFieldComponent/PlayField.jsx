9cdc0cf3ebceab24960e3e9c4a88ea13ae0aee23
GheorgheAlex <galex.marius@gmail.com>
2022-09-22 16:06:54 +0300

    Finishing design of the app

--- src/components/PlayFieldComponent/PlayField.jsx
+++ src/components/PlayFieldComponent/PlayField.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./PlayField.css";
+import cardBack from "../../assets/card-back.png";
 
 const PlayField = () => {
   return (
     <div className="play-field-container">
       <div className="play-field-deck">
-        <h3>Deck</h3>
+        {/*<h3>Deck</h3>*/}
+        <img src={cardBack} alt="card-back" />
       </div>
       <div className="play-field-pile">
         <h3>Pile of cards</h3>
